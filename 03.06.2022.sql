21e72943a7367e72253e955fedca00595fb71b83
Darbedarmoc <73374918+Darbedarmoc@users.noreply.github.com>
2022-06-03 11:50:34 +0700

    Update 03.06.2022.sql

--- 03.06.2022.sql
+++ 03.06.2022.sql
@@ -1,1 +1,5 @@
 SELECT titles.title, MAX(salaries.salary) FROM titles INNER JOIN salaries ON titles.emp_no = salaries.emp_no GROUP BY titles.title;
+
+
+SELECT departments.name, dept_emp.emp_no, MAX(salaries.salary) FROM departments, dept_emp INNER JOIN salaries 
+ON dept_emp.emp_no = salaries.emp_no AND dept_emp.dept_no = departments.dept_no GROUP BY dept_emp.emp_no 
